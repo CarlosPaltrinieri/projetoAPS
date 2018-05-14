d8706e00d624b31f8fd0bea41a69dc20efa6049b
CarlosPaltrinieri <carlospaltrinieri@hotmail.com>
2018-05-14 19:02:52 -0300

    Add files via upload

--- src/projetoaps/Ex5_Carlos.java
+++ src/projetoaps/Ex5_Carlos.java
@@ -1,4 +1,3 @@
-
 package projetoaps;
 
 import javax.swing.JOptionPane;
@@ -11,26 +10,25 @@ public class Ex5_Carlos {
         qntConj = Integer.parseInt(JOptionPane.showInputDialog("Bem vindo ao cálculo de conjuntos numéricos! \n"
                 + "Digite a quantidade de conjuntos que vc quer cálcular"));
 
-        
-            for(; count < qntConj; count++) {
-                numConj[0] = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número do " + (count + 1) + "º conjunto: "));
-                numConj[1] = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número do " + (count + 1) + "º conjunto: "));
-                numConj[2] = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro número do " + (count + 1) + "º conjunto: "));
+        for (; count < qntConj; count++) {
+            numConj[0] = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número do " + (count + 1) + "º conjunto: "));
+            numConj[1] = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número do " + (count + 1) + "º conjunto: "));
+            numConj[2] = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro número do " + (count + 1) + "º conjunto: "));
+            
+            if (numConj[0] < numConj[1] || numConj[0] < numConj[2]) {
+                res[0] = (numConj[0] + numConj[1] + numConj[2]);
+                res[1] = (numConj[0] * numConj[1] * numConj[2]);
+                res[2] = (res[0] / 3);
+
+                JOptionPane.showMessageDialog(null, "A soma do " + (count + 1) + "º conjunto é : " + res[0] + "\n"
+                        + "O produto do " + (count + 1) + "º conjunto é: " + res[1] + "\n"
+                        + "A média do " + (count + 1) + "º conjunto é: " + res[2]);
+            } else {
+                JOptionPane.showMessageDialog(null, "O valor do conjunto não está em forma crescente");
+                break;
             }
-		
-			if(numConj[0] < numConj[1] || numConj[0] < numConj[2]){
-				res[0] = (numConj[0] + numConj[1] + numConj[2]);
-				res[1] = (numConj[0] * numConj[1] * numConj[3]);
-				res[2] = (res[0] / 3);
-			
-				JOptionPane.showMessageDialog(null,"A soma do " + (count + 1) + "º conjunto é : " + res[0] + "\n" +
-										"O produto do " + (count + 1) + "º conjunto é: " + res[1] + "\n" +
-										"A média do " + (count + 1) + "º conjunto é: " + res[2]);
-			} else {
-				JOptionPane.showMessageDialog(null, "O valor do conjunto não está em forma crescente");
-				break;
-			}
-									
+        }
+        JOptionPane.showMessageDialog(null, "Obrigado por calcular conosco!");
     }
 
 }
